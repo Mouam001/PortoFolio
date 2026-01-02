c5afe0c48c14353f2ef3b17b742201bba5b395b4
Mouammar Soule <mouammar8080ni@gmail.com>
2026-01-02 22:19:16 +0100

    "Maj deploiement"

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/PortoFolio/',
   plugins: [react()],
-  base: "/PortoFolio",
   test :{
     coverage: {
       all: true,
