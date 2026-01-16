0ce80288783351c64f424c36a1f325e18d94a520
Mouammar Soule <mouammar8080ni@gmail.com>
2026-01-16 23:49:16 +0100

    "Fix : CV maj et profile"

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,6 @@
 import HomeView from "./view";
 import profileImg from "../../assets/image.png";
-import cvFile from "../../assets/cv.pdf";
+import cvFile from "../../assets/CV_Mouammar.pdf";
 
 export default function Home() {
     const profile = {
@@ -8,9 +8,10 @@ export default function Home() {
         role: "Développeur & Technicien Informatique",
         availability: "Disponible pour nouvelles opportunités",
         description: `
-Je développe des applications web et des outils métier pensés pour durer.
-En alternance, j'allie développement sur Power Platform et gestion de parc informatique. Je conçois et maintiens des applications web et des outils métier robustes, tout en assurant un support technique de proximité. Mon objectif : créer des solutions digitales pérennes qui répondent précisément aux besoins opérationnels.
-        `,
+Développeur logiciel spécialisé dans la conception d’applications métiers et de solutions web pérennes, avec une double expertise en développement et support IT.
+En alternance, j’interviens sur l’ensemble du cycle de vie des applications (conception, développement, maintenance), en m’appuyant sur des technologies comme React, .NET et la Power Platform, tout en assurant la gestion et l’optimisation des environnements informatiques.
+Mon approche est guidée par la qualité du code, la fiabilité et l’adéquation des solutions avec Git.
+`,
         location: "Calais, France — ULCO",
         experience: "plus de 2 ans d’expérience",
         image: profileImg,
